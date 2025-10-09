bf991948f5c4d834854338c7553773a727a8dafb
Henrique Mendes <henriquejesusmendesmachadoo@gmail.com>
2025-10-09 14:31:41 -0300

    Atualizaçoes feitas em cima do html e css

--- View/css/modal-vagas.js
+++ View/css/modal-vagas.js
@@ -1,27 +1,16 @@
-// Seleciona todos os botões que abrem modais
 const openButtons = document.querySelectorAll('.open-modal');
+const closeButtons = document.querySelectorAll('.close-modal');
 
-openButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modalId = button.getAttribute('data-modal');
-        const modal = document.getElementById(modalId);
-
-        if (modal) {
-            modal.showModal();
-        }
+openButtons.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        const modalId = btn.getAttribute('data-modal');
+        document.getElementById(modalId).showModal();
     });
 });
 
-// Seleciona todos os botões que fecham modais
-const closeButtons = document.querySelectorAll('.close-modal');
-
-closeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modalId = button.getAttribute('data-modal');
-        const modal = document.getElementById(modalId);
-
-        if (modal) {
-            modal.close();
-        }
+closeButtons.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        const modalId = btn.getAttribute('data-modal');
+        document.getElementById(modalId).close();
     });
 });
